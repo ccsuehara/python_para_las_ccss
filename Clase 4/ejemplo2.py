23e5b4b1935392fec2d08d9b5e915acfa9db912a
Carla Solis Uehara <tromayans@gmail.com>
2022-02-09 18:52:54 -0500

    clase 4 lista

---
+++ Clase 4/ejemplo2.py
@@ -0,0 +1,44 @@
+def eleva_potencia_cc(a,b):
+    '''
+    Eleva número "a" a la "b" potencia.
+    Insumo (input): 
+        a: número
+        b: número
+    Producto (output):
+        resultado: un número
+    '''
+    z_var = 11
+    c = 10 + z_var
+    resultado = a**b + c
+    return resultado
+
+
+
+def eleva_potencia_dd(a,b):
+    '''
+    Eleva número "a" a la "b" potencia.
+    Insumo (input): 
+        a: número
+        b: número
+    Producto (output):
+        resultado: un número
+    '''
+    z_var = 11
+    c = 10 + z_var
+    resultado = a**b + c
+    return resultado
+
+
+def eleva_potencia_ee(a,b):
+    '''
+    Eleva número "a" a la "b" potencia.
+    Insumo (input): 
+        a: número
+        b: número
+    Producto (output):
+        resultado: un número
+    '''
+    z_var = 11
+    c = 10 + z_var
+    resultado = a**b + c
+    return resultado
